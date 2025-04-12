0012cef9f86209399dcaa82bddea593c23818e91
Ahmed2005-CODE <ahmedkhalil555333@gmail.com>
2025-04-12 11:51:45 +0200

    frist abould

---
+++ source/repos/hello/hello/hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    int sum = 0;
+
+    for (int x = 1; x <= 10; x++) {
+        cout << x << endl;
+        sum += x;
+    }
+    cout << sum << endl;
+
+
+
+
+    return 0;
+
+}
+
